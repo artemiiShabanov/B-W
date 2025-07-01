2da8f55944890a5290f55f59bb6664f08fb0e142
Artemii <artemshabanov7@mail.ru>
2025-07-01 21:52:59 +0300

    touch control

--- b-w/entities/Player/player.gd
+++ b-w/entities/Player/player.gd
@@ -5,13 +5,21 @@ signal will_move(from_position)
 var animation_speed = 5
 var fast_animation_speed = 10
 var tile_size = 64
-var moving = false
 var inputs = {"right": Vector2.RIGHT,
 	"left": Vector2.LEFT,
 	"up": Vector2.UP,
 	"down": Vector2.DOWN}
-			
-			
+var states = {"right": State.RIGHT,
+	"left": State.LEFT,
+	"up": State.UP,
+	"down": State.DOWN}
+
+enum State {STALE, TRANSITION, RTRANSITION, LEFT, UP, RIGHT, DOWN}
+
+var state: State = State.STALE
+var sliding_dir_input = null
+var is_sliding = false
+
 @onready var ray = $RayCast2D
 @onready var track_ray = $TrackRayCast2D
 @onready var whoosh_sound = $WhooshSound
@@ -22,56 +30,94 @@ func _ready():
 	position = position.snapped(Vector2.ONE * tile_size)
 	position += Vector2.ONE * tile_size/2
 	anim.play("stale")
-	
-#func _input(event):
-	#for key in inputs.keys():
-		#if Input.is_action_just_pressed(key):
-			#track_ray.target_position = inputs[key] * tile_size
-			#track_ray.force_raycast_update()
-			#if track_ray.is_colliding():
-				#handle_dir(key, true)
-			#else:
-				#handle_dir(key, false)
+
+func _input(event):
+	for dir in inputs.keys():
+		if Input.is_action_just_pressed(dir):
+			step(dir)
 
 func _physics_process(delta: float) -> void:
-	for key in inputs.keys():
-		if Input.is_action_pressed(key):
-			track_ray.target_position = inputs[key] * tile_size
-			track_ray.force_raycast_update()
-			if track_ray.is_colliding():
-				handle_dir(key, true)
-			else:
-				handle_dir(key, false)
+	if !is_sliding && sliding_dir_input != null:
+		await slide(sliding_dir_input)
+	
+	for dir in inputs.keys():
+		if Input.is_action_pressed(dir):
+			sliding_dir_input = dir
 
-func handle_dir(dir, fast):
-	if moving:
+func _on_touch_handler_hold_detected(dir: Variant) -> void:
+	print(dir)
+	sliding_dir_input = dir
+
+func _on_touch_handler_swipe_detected(dir: Variant) -> void:
+	step(dir)
+
+func _on_touch_handler_release_detected() -> void:
+	sliding_dir_input = null
+	stop_slide()
+
+func step(dir):
+	if state != State.STALE || !can_move_in(dir):
 		return
-	move(dir, fast)
+	
+	state = State.TRANSITION
+	anim.play("transition")
+	await anim.animation_finished
+	
+	state = states[dir]
+	await move(dir, false)
+	
+	state = State.RTRANSITION
+	anim.play("r_transition")
+	await anim.animation_finished
+	
+	anim.play("stale")
+	state = State.STALE
 
-func move(dir, fast):
+func slide(dir):
+	if !can_move_in(dir) || !check_ray(track_ray, dir):
+		return
+	is_sliding = true
+	if ![State.LEFT, State.RIGHT, State.UP, State.DOWN].has(state):
+		state = State.TRANSITION
+		anim.play("transition")
+		await anim.animation_finished
+	if states[dir] != state:
+		anim.play(dir)
+		state = states[dir]
+	
+	await move(dir, true)
+	is_sliding = false
+	
+func stop_slide():
+	if ![State.LEFT, State.RIGHT, State.UP, State.DOWN].has(state):
+		return
+	state = State.RTRANSITION
+	anim.play("r_transition")
+	await anim.animation_finished
+	
+	anim.play("stale")
+	state = State.STALE
+
+func can_move_in(dir) -> bool:
+	return !check_ray(ray, dir)
+	
+func check_ray(ray, dir) -> bool:
 	ray.target_position = inputs[dir] * tile_size
 	ray.force_raycast_update()
-	if !ray.is_colliding():
-		moving = true
-		if fast:
-			whoosh_sound.pitch_scale = 0.8
-		else:
-			whoosh_sound.pitch_scale = 0.6
-		whoosh_sound.play()
-		if !fast:
-			anim.play("transition")
-			await anim.animation_finished
-		if anim.animation != dir:
-			anim.play(dir)
-		will_move.emit(global_position)
-		var tween = get_tree().create_tween()
-		var new_pos = position + inputs[dir] * tile_size
-		var time = 1.0 / (fast_animation_speed if fast else animation_speed)
-		tween.tween_property(self, "position", new_pos, time).set_trans(Tween.TRANS_SINE)
-		await tween.finished
-		moving = false
-		if !fast:
-			anim.play("r_transition")
-			await anim.animation_finished
-		anim.play("stale")
-		
+	return ray.is_colliding()
+
+func move(dir, fast):
+	# sound
+	if fast:
+		whoosh_sound.pitch_scale = 0.8
+	else:
+		whoosh_sound.pitch_scale = 0.6
+	whoosh_sound.play()
+	
+	# position
+	will_move.emit(position)
+	var tween = get_tree().create_tween()
+	var new_pos = position + inputs[dir] * tile_size
+	var time = 1.0 / (fast_animation_speed if fast else animation_speed)
+	tween.tween_property(self, "position", new_pos, time).set_trans(Tween.TRANS_SINE)
+	await tween.finished
---
+++ b-w/global/TouchHandler/touch_handler.gd
@@ -0,0 +1,61 @@
+extends Node2D
+
+# Constants for swipe detection
+const SWIPE_THRESHOLD = 50  # Minimum distance for a swipe to be recognized
+const HOLD_THRESHOLD = 0.5  # Minimum time in seconds for a hold to be recognized
+
+# Signals for swipe and hold gestures
+signal swipe_detected(direction)
+signal hold_detected(direction)
+signal release_detected()
+
+# Variables to track touch state
+var touch_start_position = Vector2()
+var touch_start_time = 0.0
+var prev_dir = null
+var is_holding = false
+
+func _ready():
+	# Enable input processing
+	set_process_input(true)
+
+func _input(event):
+	if event is InputEventScreenTouch:
+		if event.is_pressed():
+			# Touch started
+			touch_start_position = event.position
+			touch_start_time = Time.get_ticks_msec() / 1000.0
+			is_holding = true
+		else:
+			# Touch ended
+			var touch_end_position = event.position
+			var touch_duration = (Time.get_ticks_msec() / 1000.0) - touch_start_time
+			is_holding = false
+			prev_dir = null
+			emit_signal("release_detected")
+
+			# Check for swipe
+			var swipe_vector = touch_end_position - touch_start_position
+			if swipe_vector.length() >= SWIPE_THRESHOLD:
+				var direction = get_swipe_direction(swipe_vector)
+				emit_signal("swipe_detected", direction)
+
+	elif event is InputEventScreenDrag and is_holding:
+		# Update hold state if dragging
+		var touch_end_position = event.position
+		var current_position = event.position
+		var swipe_vector = touch_end_position - touch_start_position
+		if (Time.get_ticks_msec() / 1000.0) - touch_start_time >= HOLD_THRESHOLD:
+			var direction = get_swipe_direction(swipe_vector)
+			if direction != prev_dir:
+				emit_signal("hold_detected", direction)
+				prev_dir = direction
+			#touch_start_position = touch_end_position
+			#is_holding = false  # Prevent multiple hold signals
+
+func get_swipe_direction(swipe_vector: Vector2) -> String:
+	# Determine the direction of the swipe
+	if abs(swipe_vector.x) > abs(swipe_vector.y):
+		return "right" if swipe_vector.x > 0 else "left"
+	else:
+		return "down" if swipe_vector.y > 0 else "up"
